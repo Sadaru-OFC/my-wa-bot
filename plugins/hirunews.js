b85cf845c43beb81dfc1f18fa9abdf433751e44a
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-09 23:13:52 +0530

    Update hirunews.js

--- plugins/hirunews.js
+++ plugins/hirunews.js
@@ -18,17 +18,9 @@ if(config.BLOCK_JID.includes(from)) return
 const response = await axios.get('https://www.hirunews.lk/')
 const $ = cheerio.load(response.data)
 
-const title = $('body > div:nth-child(20) > div.row > div.col-sm-12.col-md-12.col-lg-6.section.order-lg-2.order-md-1.order-sm-1.order-1 > div > div.today-video > div.main-article-topic > a').text()
-const date = $('body > div:nth-child(18) > div.row > div.col-sm-12.col-md-12.col-lg-6.section.order-lg-2.order-md-1.order-sm-1.order-1 > div > div.today-video > div.ex-vd-tittle-time').text()
-const cnt = $('#article-phara > p').text()
+const title = $('body > div:nth-child(20) > div.row > div.col-sm-12.col-md-12.col-lg-6.section.order-lg-2.order-md-1.order-sm-1.order-1 > div > div.today-video').text()
 
-let msg = `${title}
-
-${date}
-
-${cnt}`    
-        
-await conn.sendMessage(from, {text : msg } , {quoted : mek} )
+console.log(title)
     
 }catch(e){
 console.log(e)
