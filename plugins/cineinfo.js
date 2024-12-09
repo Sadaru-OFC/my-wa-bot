fe74142c98e6e6992a69c705576f6eb9602ff7b8
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-09 22:14:18 +0530

    Update cineinfo.js

--- plugins/cineinfo.js
+++ plugins/cineinfo.js
@@ -1,5 +1,5 @@
-const {readEnv} = require('../lib/database')
-const {cmd , commands} = require('../command')
+const {readEnv} = require('../lib/database');
+const {cmd , commands} = require('../command');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
@@ -12,21 +12,35 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-const config = await readEnv()
-if(config.BLOCK_JID.includes(from)) return
-if(!isOwner) return reply("*_This is an owner cmd._*")
+const config = await readEnv();
+if(config.BLOCK_JID.includes(from)) return;
+if(!isOwner) return reply("*_This is an owner cmd._*");
+if(!q) return reply("*_Please give me a cinesubz.co url._*");
 
-axios.get('https://cinesubz.co/movies/time-cut-2024-sinhala-subtitles/')
-  .then(response => {
-    const $ = cheerio.load(response.data);
-    const title = $('title').text();
-    console.log(title);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+const response = await axios.get(`${q}`);
+const $ = cheerio.load(response.data);
 
- 
+const title = $('#single > div.content.right > div.sheader > div.data > h1');
+const date = $('#single > div.content.right > div.sheader > div.data > div.extra > span.date');
+const cnt = $('#single > div.content.right > div.sheader > div.data > div.extra > span.country');
+const dur = $('#single > div.content.right > div.sheader > div.data > div.extra > span.runtime');
+const rate = $('#repimdb > strong');
+const img = $('#info > div:nth-child(2) > span > p:nth-child(1) > img src');
+
+let msg = `🍟 ${title}
+
+🧿 Release Date : ${date}
+
+🌍 Country : ${cnt}
+
+⏱ Duration : ${dur}
+
+⭐ IMDB Rate : ${rate}`    
+        
+await conn.sendMessage(from, {text : msg } , {quoted : mek} );
+
+console.log(img);
+    
 }catch(e){
 console.log(e)
 reply(`${e}`)
