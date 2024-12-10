f6cb6ba79bf11c0731d5d8ccfac561faec3a0123
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-10 21:21:14 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,6 +1,5 @@
 const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
-const mime = require('mime-types')
 
 cmd({
     pattern: "test",
@@ -13,15 +12,19 @@ try{
 const config = await readEnv()
 if(config.BLOCK_JID.includes(from)) return
 
-function getMimeType(url) {
-    const type = mime.lookup(url)
-    return type
+const url = 'https://09.cscloud12.online/cscloud?file=Nzg0xFED5800B537160029B3B9325981d92d1Ab8727A3Vltk4xOyZPINkil%2FMsEMg4xVwez&expiry=zNHD%2Bf85JywTzoM%2FiTG6Pg%3D%3D&mac=cb825d4db6e8be1ed8074bb76cafa04ad227c6668b49ca8131e36773f0992a34&acc=5';
+
+const xhr = new XMLHttpRequest();
+xhr.open('HEAD', url, true);
+xhr.onreadystatechange = function () {
+    if(xhr.readyState === XMLHttpRequest.HEADERS_RECEIVED) {
+        const contentType = xhr.getResponseHeader('content-type');
+        console.log('MIME type:', contentType);
+    }
 }
+xhr.send();
 
-const url = 'https://09.cscloud12.online/cscloud?file=Nzg0xFED5800B537160029B3B9325981d92d1Ab8727A3Vltk4xOyZPINkil%2FMsEMg4xVwez&expiry=zNHD%2Bf85JywTzoM%2FiTG6Pg%3D%3D&mac=cb825d4db6e8be1ed8074bb76cafa04ad227c6668b49ca8131e36773f0992a34&acc=5'
-const mimeType = getMimeType(url);
 
-console.log(`MIME type of url is: ${mimeType}`)
 
 }catch(e){
 console.log(e)
