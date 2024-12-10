72c60ed33c6eca115657f929d1db9f0a9ef6470c
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-10 22:10:24 +0530

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -18,27 +18,12 @@ if(config.BLOCK_JID.includes(from)) return
 let response = await axios.get('https://www.ada.lk/')
 let $ = cheerio.load(response.data)
 let url = $('#breakingnewsads > div:nth-child(1) > a').attr('href')
-    console.log(url)
 let result = await axios.get(`${url}`)
 $ = cheerio.load(result.data)
 
 const title = $('#main > div > div.row.mainrow.ng-scope > div.inner-special.col-lg-8.col-md-12.col-12.col-sm-12.ng-scope > div > div > div.single-left-grid > h1').text()
-const date = $('#main > div > div.row.mainrow.ng-scope > div.inner-special.col-lg-8.col-md-12.col-12.col-sm-12.ng-scope > div > div > div.single-left-grid > div.reco-detail > span.sr-date').text()
-const time = $('#main > div > div.row.mainrow.ng-scope > div.inner-special.col-lg-8.col-md-12.col-12.col-sm-12.ng-scope > div > div > div.single-left-grid > div.reco-detail > span.sr-time').text()
-const desc = $('#main > div > div.row.mainrow.ng-scope > div.inner-special.col-lg-8.col-md-12.col-12.col-sm-12.ng-scope > div > div > div.single-left-grid > div.single-body-wrap > p:nth-child(2)').text()
-const img = $('#main > div > div.row.mainrow.ng-scope > div.inner-special.col-lg-8.col-md-12.col-12.col-sm-12.ng-scope > div > div > div.single-left-grid > div.single-body-wrap > p:nth-child(8) > img').attr('src');
 
-let msg = `*${title}*
-
-${date}${time}
-
-${desc}
-
-> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
-
-await conn.sendMessage(from, {text:msg},{quoted:mek})
-
-console.log(img)
+console.log(title)
     
 }catch(e){
 console.log(e)
