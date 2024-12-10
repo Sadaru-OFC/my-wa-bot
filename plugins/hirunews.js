02ab28acb65a0faa12cf30fcd34e3bb3b07bfd5d
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-10 12:21:01 +0530

    Update hirunews.js

--- plugins/hirunews.js
+++ plugins/hirunews.js
@@ -15,16 +15,16 @@ try{
 const config = await readEnv()
 if(config.BLOCK_JID.includes(from)) return
 
-let response = await axios.get('https://www.hirunews.lk/');
-let $ = cheerio.load(response.data);
-let url = $('#article-phara > p > a').attr('href');
-let result = await axios.get(`${url}`);
-$ = cheerio.load(result.data);
+let response = await axios.get('https://www.hirunews.lk/')
+let $ = cheerio.load(response.data)
+let url = $('#article-phara > p > a').attr('href')
+let result = await axios.get(`${url}`)
+$ = cheerio.load(result.data)
 
 const title = $('body > div:nth-child(17) > center > h1').text()
 const date = $('body > div:nth-child(17) > center > p').text()
 const desc = $('#article-phara2').text()
-const img = $("body > div  > div.row > div.col-sm-12.col-md-9.col-lg-9.section > div.main-article-section > div.main-article-banner > img").attr("src")
+const img = $("body > div:nth-child(19) > div.row > div.col-sm-12.col-md-9.col-lg-9.section > div.main-article-section > div.main-article-banner > img").attr("src")
 
 let msg = `*${title}*
 
@@ -33,7 +33,9 @@ ${desc}
 
 > ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
 
-await conn.sendMessage(from, {image:{url:img},caption:msg},{quoted:mek})
+await conn.sendMessage(from, {text:msg},{quoted:mek})
+
+console.log(img)
     
 }catch(e){
 console.log(e)
