1d722503792b20c1fd0ca3f9c43555aa6e73c309
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-10 22:27:08 +0530

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -15,10 +15,10 @@ try{
 const config = await readEnv()
 if(config.BLOCK_JID.includes(from)) return
 
-let response = await axios.get('https://wabetainfo.com/')
+let response = await axios.get('https://wabetainfo.com/android/')
 let $ = cheerio.load(response.data)
 
-const time = $('#post-35963 > div > div.entry-metas.mb-half-gutter.last\:mb-0 > span.meta-item.posted-on > span > time').text()
+const time = $('#post-35897 > div > div.entry-metas.mb-half-gutter.last\:mb-0 > span.meta-item.posted-on > span > time').text()
 
 console.log(time)
     
