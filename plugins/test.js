3fe29d13870c3b254cc879dc38b2a35296e46c12
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-10 21:14:16 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,5 +1,6 @@
 const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
+const mime = require('mime-types')
 
 cmd({
     pattern: "test",
@@ -12,19 +13,15 @@ try{
 const config = await readEnv()
 if(config.BLOCK_JID.includes(from)) return
 
-const url = "https://09.cscloud12.online/cscloud?file=Nzg0xFED5800B537160029B3B9325981d92d1Ab8727A3Vltk4xOyZPINkil%2FMsEMg4xVwez&expiry=zNHD%2Bf85JywTzoM%2FiTG6Pg%3D%3D&mac=cb825d4db6e8be1ed8074bb76cafa04ad227c6668b49ca8131e36773f0992a34&acc=5";
-
-function getFileNameFromUrl(url) {
-    const indexOfLastSlash = url.lastIndexOf("/");
-    const urlWithoutSlash = url.substring(0, indexOfLastSlash);
-    const fileNameWithQueryParams = urlWithoutSlash.substring(urlWithoutSlash.lastIndexOf("/") + 1);
-    const fileName = fileNameWithQueryParams.split("?")[0];
-    
-    return fileName;
+function getMimeType(url) {
+    const type = mime.lookup(url)
+    return type
 }
 
-const fileName = getFileNameFromUrl(url);
-console.log(fileName);
+const url = 'https://09.cscloud12.online/cscloud?file=Nzg0xFED5800B537160029B3B9325981d92d1Ab8727A3Vltk4xOyZPINkil%2FMsEMg4xVwez&expiry=zNHD%2Bf85JywTzoM%2FiTG6Pg%3D%3D&mac=cb825d4db6e8be1ed8074bb76cafa04ad227c6668b49ca8131e36773f0992a34&acc=5'
+const mimeType = getMimeType(url);
+
+console.log(`MIME type of url is: ${mimeType}`)
 
 }catch(e){
 console.log(e)
