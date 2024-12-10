aed043e3a3d8acc9b87d281e2210b2ec22e45a87
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-10 21:57:48 +0530

    Update and rename hirunews.js to news.js

--- plugins/news.js
+++ plugins/news.js
@@ -27,7 +27,7 @@ $ = cheerio.load(results.data)
 const title = $('#main > div > div.row.mainrow.ng-scope > div.inner-special.col-lg-8.col-md-12.col-12.col-sm-12.ng-scope > div > div > div.single-left-grid > h1').text()
 const date = $('#main > div > div.row.mainrow.ng-scope > div.inner-special.col-lg-8.col-md-12.col-12.col-sm-12.ng-scope > div > div > div.single-left-grid > div.reco-detail > span.sr-date').text()
 const desc = $('#main > div > div.row.mainrow.ng-scope > div.inner-special.col-lg-8.col-md-12.col-12.col-sm-12.ng-scope > div > div > div.single-left-grid > div.single-body-wrap > p:nth-child(2)').text()
-const img = $('#main > div > div.row.mainrow.ng-scope > div.inner-special.col-lg-8.col-md-12.col-12.col-sm-12.ng-scope > div > div > div.single-left-grid > div.single-body-wrap > p:nth-child(8) > img').attr('src');
+const img = $('#main > div > div.row.mainrow.ng-scope > div.inner-special.col-lg-8.col-md-12.col-12.col-sm-12.ng-scope > div > div > div.single-left-grid > div.single-body-wrap > p:nth-child(8)').attr('src');
 
 let msg = `*${title}*
 
@@ -37,7 +37,9 @@ ${desc}
 
 > ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
 
-await conn.sendMessage(from, {image: {url:img},caption:msg},{quoted:mek})
+await conn.sendMessage(from, {text:msg},{quoted:mek})
+
+console.log(img)
     
 }catch(e){
 console.log(e)
