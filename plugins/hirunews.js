a91fcf1f168c42eab6fe9442e954c761dadb753f
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-10 21:55:07 +0530

    Update hirunews.js

--- plugins/hirunews.js
+++ plugins/hirunews.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 
 cmd({
-    pattern: "hirunews",
+    pattern: "news",
     desc: "Get hirunews",
     category: "other",
     filename: __filename
@@ -15,27 +15,29 @@ try{
 const config = await readEnv()
 if(config.BLOCK_JID.includes(from)) return
 
-let response = await axios.get('https://www.hirunews.lk/')
+let response = await axios.get('https://www.ada.lk/')
 let $ = cheerio.load(response.data)
-let url = $('#article-phara > p > a').attr('href')
+let url = $('#navbarNav > ul > li:nth-child(2) > a').attr('href')
 let result = await axios.get(`${url}`)
 $ = cheerio.load(result.data)
+let link = $('body > div:nth-child(3) > div > div.col-lg-6.col-md-6.col-sm-12.category-text-block > div > div.cat-text-primary > h2 > strong > a').attr('href')
+let results = await axios.get(`${link}`)
+$ = cheerio.load(results.data)
 
-const title = $('body > div:nth-child(17) > center > h1').text()
-const date = $('body > div:nth-child(17) > center > p').text()
-const desc = $('#article-phara2').text()
-const img = $('body > div:nth-child(19) > div.row > div.col-sm-12.col-md-9.col-lg-9.section > div.main-article-section > div.main-article-banner > img').attr('src');
+const title = $('#main > div > div.row.mainrow.ng-scope > div.inner-special.col-lg-8.col-md-12.col-12.col-sm-12.ng-scope > div > div > div.single-left-grid > h1').text()
+const date = $('#main > div > div.row.mainrow.ng-scope > div.inner-special.col-lg-8.col-md-12.col-12.col-sm-12.ng-scope > div > div > div.single-left-grid > div.reco-detail > span.sr-date').text()
+const desc = $('#main > div > div.row.mainrow.ng-scope > div.inner-special.col-lg-8.col-md-12.col-12.col-sm-12.ng-scope > div > div > div.single-left-grid > div.single-body-wrap > p:nth-child(2)').text()
+const img = $('#main > div > div.row.mainrow.ng-scope > div.inner-special.col-lg-8.col-md-12.col-12.col-sm-12.ng-scope > div > div > div.single-left-grid > div.single-body-wrap > p:nth-child(8) > img').attr('src');
 
 let msg = `*${title}*
 
 ${date}
+
 ${desc}
 
 > ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
 
-await conn.sendMessage(from, {text:msg},{quoted:mek})
-
-console.log(img);
+await conn.sendMessage(from, {image: {url:img},caption:msg},{quoted:mek})
     
 }catch(e){
 console.log(e)
