38d31098cb8396767c931ce7687d28f04558a844
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-15 13:52:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     
 SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=uipClADK#TopMVXyrDmHa5OBfsn95a4Rsl-86_6NU_Us593uv72E",
-MONGODB: process.env.MONGODB || "mongodb://mongo:ZNQtexHcMQJddDjdpPnBYThOBsYzEjDr@junction.proxy.rlwy.net:55587",
+MONGODB: process.env.MONGODB || "mongodb://mongo:mblQtZazdYwrTCvIULqayMncfvEZpgPv@junction.proxy.rlwy.net:34056",
     
 };
