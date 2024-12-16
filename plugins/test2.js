8aea32b1e0b8e0c5da111029fe9fa072d45aba6f
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-16 12:36:03 +0530

    Create test2.js

---
+++ plugins/test2.js
@@ -0,0 +1,47 @@
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
+const puppeteer = require('puppeteer')
+
+cmd({
+    pattern: "test2",
+    desc: "test",
+    category: "other",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+const config = await readEnv()
+if(config.BLOCK_JID.includes(from)) return
+
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+
+// Navigate the page to a URL.
+await page.goto('https://developer.chrome.com/');
+
+// Set screen size.
+await page.setViewport({width: 1080, height: 1024});
+
+// Type into search box.
+await page.locator('.devsite-search-field').fill('automate beyond recorder');
+
+// Wait and click on first result.
+await page.locator('.devsite-result-item-link').click();
+
+// Locate the full title with a unique string.
+const textSelector = await page
+  .locator('text/Customize and automate')
+  .waitHandle();
+const fullTitle = await textSelector?.evaluate(el => el.textContent);
+
+// Print the full title.
+console.log('The title of this blog post is "%s".', fullTitle);
+
+await browser.close();
+  
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
