f918190ee0becdf139e0d414c429100447bded06
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-17 11:10:00 +0530

    Update test2.js

--- plugins/test2.js
+++ plugins/test2.js
@@ -14,13 +14,26 @@ try{
 
 const config = await readEnv()
 if(config.BLOCK_JID.includes(from)) return
-
-let response = await axios.get(`https://cinesubz.co/?s=2024`)
+if(!q) return
+    
+let response = await axios.get(`https://cinesubz.co/?s=${q}`)
 let $ = cheerio.load(response.data)
     
-let title = $('#contenedor > div.module > div.content.rigth.csearch > div.search-page > div:nth-child(2)')
+let title = $(#contenedor > div.module > div.content.rigth.csearch > div.search-page > div:nth-child(2) > article > div.details > div.title > a).text()
+
+let image = $(#contenedor > div.module > div.content.rigth.csearch > div.search-page > div:nth-child(2) > article > div.image > div > a > img).attr('src')
+
+let link = $(#contenedor > div.module > div.content.rigth.csearch > div.search-page > div:nth-child(2) > article > div.image > div > a').attr('href')
+
+let rating = $('#contenedor > div.module > div.content.rigth.csearch > div.search-page > div:nth-child(2) > article > div.details > div.meta > span:nth-child(1)').text()
+
+let year = $('#contenedor > div.module > div.content.rigth.csearch > div.search-page > div:nth-child(2) > article > div.details > div.meta > span.year').text()
     
-console.log(title)
+    console.log(title)
+    console.log(image)
+    console.log(year)
+    console.log(rating)
+    console.log(link)
        
     
 }catch(e){
