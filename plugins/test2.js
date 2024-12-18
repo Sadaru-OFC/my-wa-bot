c2c3f6644f4e7ad5707d6c4f1a75c47ac8dbc17a
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-18 23:03:25 +0530

    Update test2.js

--- plugins/test2.js
+++ plugins/test2.js
@@ -1,45 +1,55 @@
-const { readEnv } = require('../lib/database');
-const { cmd, commands } = require('../command');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
+const axios = require('axios')
+const cheerio = require('cheerio')
 
 cmd({
-  pattern: "test2",
-  desc: "test",
-  category: "owner",
-  filename: __filename,
-}, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-  try {
-    const config = await readEnv();
-    if (config.BLOCK_JID.includes(from)) return;
+    pattern: "test2",
+    desc: "test",
+    category: "owner",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
 
-    const url = `https://cinesubz.co/?s=${q}`;
-    const moviesData = {};
+const config = await readEnv()
+if(config.BLOCK_JID.includes(from)) return
 
-    async function getHTML() {
-      const { data: html } = await axios.get(url, {
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-        },
-      });
-      return html;
-    }
+const url = `https://cinesubz.co/?s=${q}`;
 
-    getHTML().then((res) => {
-      const $ = cheerio.load(res);
-$('#contenedor > div.module > div.content.rigth.csearch > div.search-page').each((i, movie) => {
-  const title = $(movie)
+const moviesData = {};
+
+async function getHTML () {
+  const { data: html } = await axios.get(url, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+    }
+  });
+  return html;
+};
+
+getHTML().then((res) => {
+const $ = cheerio.load(res);
+  
+const titles = $('#contenedor > div.module > div.content.rigth.csearch > div.search-page')
   .find('.title > a')
   .map((i, el) => $(el).text().trim())
   .get()
-  .join(' , '); 
+  .join('\n');
+
+const ratings = $('#contenedor > div.module > div.content.rigth.csearch > div.search-page')
+  .find('.meta > span:nth-child(1)')
+  .map((i, el) => $(el).text().trim())
+  .get()
+  .join('\n');
+
+console.log(titles);
+console.log(ratings);
 
 });
-console.log(title)
-});
-    
-  } catch (e) {
-    console.log(e);
-    reply(`${e}`);
-  }
-});
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
