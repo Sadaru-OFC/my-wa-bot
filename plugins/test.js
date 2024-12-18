b403af0fe3b1560b6ec7e714399c9b7dd22b8eb0
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-18 18:02:15 +0530

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,45 @@
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const fs = require('fs-extra')
+
+cmd({
+    pattern: "test",
+    desc: "test",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+const config = await readEnv()
+if(config.BLOCK_JID.includes(from)) return
+
+const url = 'https://www.imdb.com/chart/top/';
+
+const moviesData = {};
+  
+async function getHTML () {
+  const { data: html } = await axios.get(url);
+  return html;
+};
+
+getHTML().then((res) => {
+const $ = cheerio.load(res);
+$('.ipc-metadata-list ipc-metadata-list--dividers-between sc-a1e81754-0 iyTDQy compact-list-view ipc-metadata-list--base').each((i, movie) => {
+  const title = $(movie).find('.ipc-title__text').text();
+  const rating = $(movie).find('.ipc-rating-star--rating').text();
+  moviesData[title] = rating;
+});
+  fs.writeFile('moviesData.json', JSON.stringify(moviesData), (err) => {
+    if (err) throw err;
+    console.log('file saved!');
+  });
+});
+  
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
