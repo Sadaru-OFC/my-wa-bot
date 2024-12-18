a1a2576637b984de6bb3f2bc060e5ec9a929d43c
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-18 19:22:45 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -36,7 +36,10 @@ $('#__next > main > div > div.ipc-page-content-container.ipc-page-content-contai
   const rating = $(movie).find('.ipc-rating-star--rating').text().trim();
   moviesData[title] = rating;
 });
-console.log(moviesData)
+fs.writeFile('/my_data/moviesData.json', JSON.stringify(moviesData), (err) => {
+    if (err) throw err;
+    console.log('File saved!');
+});
 });
   
 }catch(e){
