61bdb9a1ab0e332b892ac41c4dae43600423f5cd
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-18 09:58:14 +0530

    Update and rename test.js to sindl.js

--- plugins/sindl.js
+++ plugins/sindl.js
@@ -4,9 +4,9 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 
 cmd({
-    pattern: "test",
+    pattern: "sindl",
     desc: "test",
-    category: "other",
+    category: "download",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
@@ -14,16 +14,17 @@ try{
 
 const config = await readEnv()
 if(config.BLOCK_JID.includes(from)) return
-let emptyMsg = `*_Please give me a sinhalasub url._*`
-    
-let response = await axios.get('https://webtor.io/48dd61a1ca0572a7941a499de12bab7f057a3d12')
+let emptyMsg = `*_Please give me a sinhalasub url._*
+
+.sindl https://sinhalasub.lk/links/0ufiu8bbt9/`
+if(!q) return reply(emptyMsg)
+let response = await axios.get(q)
 let $ = cheerio.load(response.data)
 
-const title = $('#file > h1').text().trim()
-const img = $('#log-590d881d00d16d287b8137eb98c6333fe022472b > form > div > div > div > a:nth-child(4)').attr('href')
-    
-console.log(title)
-console.log(img)
+const url = $('#link > a').attr('href')
+
+reply(url)
+console.log(url)
     
 }catch(e){
 console.log(e)
