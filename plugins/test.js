e479cb6aa673cd1c314415b963cb7e1766c1ce6f
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-18 18:31:25 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -21,15 +21,15 @@ const url = 'https://www.imdb.com/chart/top/';
 const moviesData = {};
   
 async function getHTML () {
-  const { data: html } = await axios.get(url);
+  const { data: html } = await axios.get(url, { delay: 2000 }); // Add a 2-second delay
   return html;
 };
 
 getHTML().then((res) => {
 const $ = cheerio.load(res);
-$('.ipc-metadata-list ipc-metadata-list--dividers-between sc-a1e81754-0 iyTDQy compact-list-view ipc-metadata-list--base').each((i, movie) => {
-  const title = $(movie).find('.ipc-title ipc-title--base ipc-title--title ipc-title-link-no-icon ipc-title--on-textPrimary sc-a69a4297-2 bqNXEn cli-title with-margin > a > h3').text().trim();
-  const rating = $(movie).find('.ipc-rating-star--rating').text().trim();
+$('#__next > main > div > div.ipc-page-content-container.ipc-page-content-container--center > section > div > div.ipc-page-grid.ipc-page-grid--bias-left > div > ul').each((i, movie) => {
+  const title = $(movie).find('.div.ipc-title.ipc-title--base.ipc-title--title.ipc-title-link-no-icon.ipc-title--on-textPrimary.sc-a69a4297-2.bqNXEn.cli-title.with-margin > a > h3').text().trim();
+  const rating = $(movie).find('.span > div > span > span.ipc-rating-star--rating').text().trim();
   moviesData[title] = rating;
 });
   fs.writeFile('moviesData.json', JSON.stringify(moviesData), (err) => {
