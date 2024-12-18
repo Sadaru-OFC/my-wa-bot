4995cf3335d901c9da5414f2614f89d092b604e4
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-18 20:56:07 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -2,13 +2,12 @@ const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
 const axios = require('axios')
 const cheerio = require('cheerio')
-const fs = require('fs-extra')
 
 cmd({
-    pattern: "test",
-    desc: "test",
-    category: "download",
-    filename: __filename
+    pattern: "test",
+    desc: "test",
+    category: "download",
+    filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
@@ -19,28 +18,25 @@ if(config.BLOCK_JID.includes(from)) return
 const url = 'https://www.imdb.com/chart/top/';
 
 const moviesData = {};
-  
+
 async function getHTML () {
-  const { data: html } = await axios.get(url, {
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
-    }
-  });
-  return html;
+  const { data: html } = await axios.get(url, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+    }
+  });
+  return html;
 };
 
 getHTML().then((res) => {
 const $ = cheerio.load(res);
-const mappedMovies = $('#__next > main > div > div.ipc-page-content-container.ipc-page-content-container--center > section > div > div.ipc-page-grid.ipc-page-grid--bias-left > div > ul').map((i, movie) => {
-  const title = $(movie).find('.ipc-title__text').text().trim();
-  const rating = $(movie).find('.ipc-rating-star--rating').text().trim();
-  return { title, rating };
-}).get();
-    mappedMovies.forEach(movie => {
-  console.log(`Title: ${movie.title}, Rating: ${movie.rating}`);
+$('#__next > main > div > div.ipc-page-content-container.ipc-page-content-container--center > section > div > div.ipc-page-grid.ipc-page-grid--bias-left > div > ul').each((i, movie) => {
+  const title = $(movie).find('.ipc-title__text').text().trim();
+  const rating = $(movie).find('.ipc-rating-star--rating').text().trim();
 });
+reply(JSON.stringify(moviesData))
 });
-  
+
 }catch(e){
 console.log(e)
 reply(`${e}`)
