bc92bd51833f1832c064aac1245a4932dc4dc98a
Sadaru-OFC <sadarugames999@gmail.com>
2024-12-18 10:39:32 +0530

    Update sindl.js

--- plugins/sindl.js
+++ plugins/sindl.js
@@ -22,12 +22,71 @@ let response = await axios.get(q)
 let $ = cheerio.load(response.data)
 
 const url = $('#link').attr('href')
+const title = $('title').text().trim()
+const mUrl = $('body > div > div > div > div.inside > small:nth-child(3) > a').attr('href')
+    
+let result = await axios.get(`${mUrl}`)
+$ = cheerio.load(result.data)
 
-console.log(url)
+const img = $('#dt_galery > div.owl-wrapper-outer > div > div:nth-child(1) > div > a > img').attr('src')
+    
+let pmsg = `*_INFINITY WA BOT Sinhalasub.lk DOWNLOADER 📥_*
+
+*Movie name :* ${title}
+
+*Download link :* ${url}
+
+🔢 Reply Below Number :
+
+1 || Download movie
+
+> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
+
+let omsg = `*_INFINITY WA BOT Sinhalasub.lk DOWNLOADER 📥_*
+
+*Movie name :* ${title}
+
+*Download link :* ${url}
+
+> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴀʀᴜ`
+
+if(url.startsWith('https://pixeldrain.com/'))  {
+
+let newUrl = url.replace('/u/', '/api/file/')
+let caption = `_${title}_
+
+> ɪɴꜰɪɴɪᴛʏ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`
+    
+let send = await conn.sendMessage(from,{image:{url: img},caption:pmsg},{quoted:mek})
+
+conn.ev.on('messages.upsert', async (msgUpdate) => {
+            const msg = msgUpdate.messages[0];
+            if (!msg.message || !msg.message.extendedTextMessage) return;
+
+            const selectedOption = msg.message.extendedTextMessage.text.trim();
+
+            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === send.key.id) {
+                switch (selectedOption) {
+                    case '1':
+
+await conn.sendMessage(from,{document: {url: newUrl },mimetype:"video/mp4",fileName: "🎬 INFINITY WA BOT 🎬" + title + ".mp4",caption: caption},{quoted: send})
+                        
+                        break;
+                    default:
+                        reply("*_Invalid number.Please reply a valid number._*")
+				
+}
+}
+})
+    
+} else {
+
+await conn.sendMessage(from,{image:{url: img},caption: omsg},{quoted:mek})
+    
+}
     
 }catch(e){
 console.log(e)
 reply(`${e}`)
 }
 })
-
