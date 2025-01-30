1b5813482fb3dfc12311b2288509d1f31c8d0a1c
Sadaru-OFC <sadarugames999@gmail.com>
2025-01-30 07:59:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,14 +14,14 @@
 */
 
 module.exports = {
-  SESSION_ID: "", // Enter your session id
-  OWNER_NUMBER: "", // Enter your owner number (Ex :- 947018xxxxx)
+  SESSION_ID: "Infinity∞LjAkVYzZ#2fvPgqGE-BSfc8N0mJ9GKysk-7M_lE3CJPaiowj0GnY", // Enter your session id
+  OWNER_NUMBER: "94701814946", // Enter your owner number (Ex :- 947018xxxxx)
   PREFIX: ".",
-  GOOGLE_API_KEY: "", // Enter your google api key
-  MAX_SIZE: "100", // 100MB
+  GOOGLE_API_KEY: "AIzaSyDpCGEwnXmyVURfMCKSA245bNsS2NFewB8", // Enter your google api key
+  MAX_SIZE: "2500", // 100MB
   AUTO_STATUS_READ: "on", // on or off
-  MV_SEND_JID: "", // Enter your default movie send jid
-  XVDL_JID: "", // Enter your 18+ dl allow group jid
-  AUTO_AI: "", // on or off
-  AUTO_AI_JID: "", // Enter your auto AI allow group jid
+  MV_SEND_JID: "120363355439809658@g.us", // Enter your default movie send jid
+  XVDL_JID: "120363370870982845@g.us", // Enter your 18+ dl allow group jid
+  AUTO_AI: "on", // on or off
+  AUTO_AI_JID: "120363365489430422@g.us", // Enter your auto AI allow group jid
 };
